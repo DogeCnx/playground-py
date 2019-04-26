d8f3cadbce27bb71e7906cdcc63562e06bcac58c
Therapat <Saratoga1412@gmail.com>
2019-04-26 23:02:21 +0700

    New Homework Exercise 4

---
+++ Assignments/Exercise4_Therapat_S.py
@@ -0,0 +1,16 @@
+'''
+ตัวย่อรายชื่อวิชาที่ใช้เก็บตัวแปร
+Foundation English                แทน FE
+General Business                  แทน GB
+Introduction to Computer Systems  แทน ICS
+Computer Programming              แทน CP
+'''
+
+ScoreFE = 86.0
+ScoreGB = 84.5
+ScoreICS = 56.62
+ScoreCP = 44.8
+print("Foundation English               : ",ScoreFE )
+print("General Business                 : ",ScoreGB)
+print("Introduction to Computer Systems : ",ScoreICS)
+print("Computer Programming             : ",ScoreCP)
